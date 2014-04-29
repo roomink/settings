899ffce2548903fc9e9a3cbc5f85161365b807d9
Vsevolod Romashov <7@7vn.ru>
2014-04-29 17:23:38 +0400

    Settings::Utils

--- lib/settings.rb
+++ lib/settings.rb
@@ -2,6 +2,7 @@ require 'settings/version'
 require 'hashie/mash'
 require 'active_support/core_ext/hash/deep_merge'
 require 'yaml'
+require 'settings/utils'
 
 module Settings
   ENVIRONMENTS = %i(development test staging production)
@@ -29,8 +30,8 @@ module Settings
     
   private
     def method_missing(method_name, *args, &block)
-      if _mashes[_env].respond_to?(method_name)
-        _mashes[_env].send(method_name, &block)
+      if _mashes[Utils.env].respond_to?(method_name)
+        _mashes[Utils.env].send(method_name, &block)
       else
         super
       end
@@ -39,54 +40,10 @@ module Settings
     def _mashes
       @_mashes ||= begin
         ENVIRONMENTS.each_with_object(Hash.new) do |environment, hash|
-          env_settings = _load_settings_for(environment)
+          env_settings = Utils.load_settings_for(environment)
           hash[environment] = Hashie::Mash.new(env_settings)
         end
       end
-    end
-    
-    def _load_settings_for(environment)
-      _paths_for(environment).map do |path|
-        _read_file(path)
-      end.compact.inject({}, :deep_merge)
-    end
-    
-    def _paths_for(environment)
-      [
-        %w(settings.yml),
-        %W(settings #{environment}.yml),
-        %w(settings.local.yml),
-        %W(settings #{environment}.local.yml)
-      ].map do |path_parts|
-        _root.join('config', *path_parts)
-      end.select(&:exist?)
-    end
-    
-    def _read_file(path)
-      yaml = File.read(path)
-      YAML.load(yaml) unless yaml.empty?
-    end
-    
-    def _root
-      if _rails?
-        Rails.root
-      else
-        Pathname.new(Dir.pwd)
-      end
-    end
-    
-    def _env
-      if _rails?
-        Rails.env
-      elsif ENV['RAILS_ENV']
-        ENV['RAILS_ENV']
-      else
-        :development
-      end.to_sym
-    end
-    
-    def _rails?
-      const_defined?(:Rails)
     end
   end
 end
---
+++ lib/settings/utils.rb
@@ -0,0 +1,53 @@
+module Settings
+  module Utils
+    class << self
+      def load_settings_for(environment)
+        paths_for(environment).map do |path|
+          read_file(path)
+        end.compact.inject({}, :deep_merge)
+      end
+      
+      def paths_for(environment)
+        [
+          %w(settings.yml),
+          %W(settings #{environment}.yml),
+          %w(settings.local.yml),
+          %W(settings #{environment}.local.yml)
+        ].map do |path_parts|
+          root.join('config', *path_parts)
+        end.select(&:exist?)
+      end
+      
+      def read_file(path)
+        yaml = File.read(path)
+        YAML.load(yaml) unless yaml.empty?
+      end
+      
+      def env
+        if rails?
+          Rails.env
+        elsif ENV['RAILS_ENV']
+          ENV['RAILS_ENV']
+        else
+          :development
+        end.to_sym
+      end
+      
+      def rails?
+        const_defined?(:Rails)
+      end
+      
+      def root
+        rails_root || pwd
+      end
+      
+      def rails_root
+        Rails.root if rails?
+      end
+      
+      def pwd
+        Pathname.new(Dir.pwd)
+      end
+    end
+  end
+end
